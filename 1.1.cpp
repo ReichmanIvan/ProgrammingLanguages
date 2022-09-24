987958fa07c90dded7b6b0e3d5501fcb4c03978f
Иван Райхман <vantikss@MacBook-Air-Ivan.local>
2022-09-25 00:22:18 +0300

    место для решения 1 задания

---
+++ 1.1.cpp
@@ -0,0 +1,49 @@
+#define _USE_MATH_DEFINES // for C++
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+/**
+*  \brief Функция расчета по заданной формуле.
+*  \param x Первый параметр функции.
+*  \param y Второй параметр функции.
+*  \param z Третий параметр функции.
+*  \return Значение функции.
+*/ 
+double getA (double x, double y, double z);
+
+/**
+*  \brief Функция расчета по заданной формуле.
+*  \param x Первый параметр функции.
+*  \param y Второй параметр функции.
+*  \param z Третий параметр функции.
+*  \return Значение функции.
+*/ 
+double getB (double x, double y, double z);
+/**
+*  \brief Точка входа в программу.
+*  \return Возвращает 0 в случае успеха.
+*/
+int main()
+{
+const auto x = -0.5;
+const auto y = 1.7;
+const auto z = 0.44;
+const auto a = getA (x, y, z);
+const auto b = getB (x, y, z);
+
+cout << " x = " << x << ", y = " << y << ", z = " << z << "\n";
+cout << " a = " << a << ", b = " << b << endl;
+
+return 0;
+}
+
+double getA (double x, double y, double z)
+{
+return exp(-y *z ) * sin(x * z - y) - pow(abs(y * z + x), 1.0/2);
+}
+
+double getB (const double x, const double y, const double z)
+{
+return y * sin(x*z*z*cos(2 * z)) - 1;
+}
