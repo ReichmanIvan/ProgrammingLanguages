be30a8e7171ed1e2c720ce627f72a4095159bd5a
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2022-11-24 13:15:17 +0300

    Update 3.2.cpp

--- 3.2.cpp
+++ 3.2.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 using namespace std;
 
-/**
-* \brief функция определения текущего элемента ряда.
-* \param k - переменная для вычисления элемента ряда.
-* \return значение текущего элемент ряда.
+/*
+*\brief функция определения текущего элемента ряда.
+*\param k - переменная для вычисления элемента ряда.
+*\return значение текущего элемент ряда.
 */
 double GetCurrent(const size_t k);
 
-/**
+/*
 * \brief функция вычисления суммы первых n элементов ряда.
 * \param n - количество элементов ряда.
 * \param k - переменная для вычисления элемента ряда.
@@ -17,15 +17,15 @@ double GetCurrent(const size_t k);
 */
 double GetSumm(const int n);
 
-/**
+/*
 * \brief функция вычисления суммы членов ряда не меньших числа e.
 * \param e - константа по условию.
 * \param k - переменная для вычисление элемента ряда.
 * \return сумма членов ряда не меньших числа e.
 */
-double GetSumm(const double e,  size_t k);
+double GetSumm(const int n,const double e);
 
-/**
+/*
 * \brief точка входа в программу.
 * \return возвращает 0 в случае успеха.
 */
@@ -39,35 +39,35 @@ int main()
     cout << "Введите e" << endl;
     cin >> e;
     cout << GetSumm(n) << endl;
-    cout << GetSumm(e, k);
+    cout << GetSumm(n, e);
     return 0;
 }
 
 double GetCurrent(const size_t k)
 {
-    return -1 * (1.0 / pow((k + 1),2));
+    return (pow(-1,k))*(1.0 / pow((k + 1),2));
 }
 
 double GetSumm(const int n)
 {
     double sum = 0.0;
-    for (int i = 1; i <=  n; i++)
+    for (size_t k = 1; k <=  n; k++)
     {
-        size_t k=k+1;
-        sum = sum + GetCurrent(k);
+       sum = sum + GetCurrent(k);
     }
 return sum;
 }
 
-double GetSumm(const double e, size_t k)
+double GetSumm(const int n,const double e)
 {
     double sum = 0.0;
-    double current = GetCurrent(k);
-    while (abs(current) > e)
+    for (size_t k = 1; k <=  n; k++)
     {
+        double current = GetCurrent(k);
+        if (abs(current)>=e)
+        {
         sum = sum + current;
-        k++;
-        current = GetCurrent(k);
+        }
     }
     return sum;
-}
+    }
