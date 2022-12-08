35d00fdea2a8e471ded7ed1443bd43a473917eaa
Райхман Иван ТКИ-141 <113427606+ReichmanIvan@users.noreply.github.com>
2022-12-08 19:18:21 +0300

    Update 3.2.cpp

--- 3.2.cpp
+++ 3.2.cpp
@@ -12,7 +12,6 @@ double GetCurrent(const size_t k);
 /*
 * \brief функция вычисления суммы первых n элементов ряда.
 * \param n - количество элементов ряда.
-* \param k - переменная для вычисления элемента ряда.
 * \return сумма первых n элементов ряда.
 */
 double GetSumm(const int n);
@@ -20,10 +19,16 @@ double GetSumm(const int n);
 /*
 * \brief функция вычисления суммы членов ряда не меньших числа e.
 * \param e - константа по условию.
-* \param k - переменная для вычисление элемента ряда.
 * \return сумма членов ряда не меньших числа e.
 */
-double GetSumm(const int n,const double e);
+double GetSumm(const double e);
+
+/*
+ \brief функция вычисления факториала.
+* \param N - число, факториал которого нужно вычислить.
+* \return возвращает факториал.
+**/
+double GetFact(int N);
 
 /*
 * \brief точка входа в программу.
@@ -39,13 +44,23 @@ int main()
     cout << "Введите e" << endl;
     cin >> e;
     cout << GetSumm(n) << endl;
-    cout << GetSumm(n, e);
+    cout << GetSumm(e);
     return 0;
+}
+
+double GetFact(int N)
+{
+    if(N < 0) 
+        return 0; 
+    if (N == 0) 
+        return 1;
+    else 
+        return N * GetFact(N - 1);
 }
 
 double GetCurrent(const size_t k)
 {
-    return (pow(-1,k))*(1.0 / pow((k + 1),2));
+    return (pow(-1,k))*(1.0 / pow(GetFact(k),2));
 }
 
 double GetSumm(const int n)
@@ -58,16 +73,18 @@ double GetSumm(const int n)
 return sum;
 }
 
-double GetSumm(const int n,const double e)
+double GetSumm(const double e)
 {
+    int k = 1;
     double sum = 0.0;
-    for (size_t k = 1; k <=  n; k++)
+    while (abs(GetCurrent(k)) >= e)
+    
     {
-        double current = GetCurrent(k);
-        if (abs(current)>=e)
+        if (GetCurrent(k) >= e)
         {
-        sum = sum + current;
+        sum = sum + GetCurrent(k);
         }
+        k += 1;
     }
     return sum;
-    }
+}
